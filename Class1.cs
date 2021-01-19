362c30253983c2246a7b6c32bbdc7e0871ba6c0a
dippulso <dippulso96@gmail.com>
2021-01-19 20:22:04 +0600

    Initial Commit

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using bim;
+using System.Collections.Generic;
+using static bim.Form1;
+
+namespace bim
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document document = commandData.Application.ActiveUIDocument.Document;
+            FilteredElementCollector elementCollector = new FilteredElementCollector(document);
+            List<Element> elements1 = (List<Element>)new FilteredElementCollector(document, (document.ActiveView.Id)).ToElements();
+            List<Elems> elems = new List<Elems>();
+            Form1 form1 = new Form1();
+            using (List<Element>.Enumerator enumerator = elements1.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    Element current = enumerator.Current;
+                    string str = !(current.Name == "") && current.Name != null ? current.Name : "Без названия";
+                    elems.Add(new Form1.Elems()
+                    {
+                        ID = current.Id,
+                        name = str
+                    });
+                }
+            }
+            form1.ShowForm(elems);
+            return (Result)0;
+        }
+    }
+}
